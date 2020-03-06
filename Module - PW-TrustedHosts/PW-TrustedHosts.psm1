6b2886b9ea188e3806ce2dbb21b7b70cf7419990
buzzict <55189343+buzzict@users.noreply.github.com>
2020-03-06 11:36:51 +0100

    Add Module PW-TrustedHosts.psm1

---
+++ Module - PW-TrustedHosts/PW-TrustedHosts.psm1
@@ -0,0 +1,41 @@
+################## ADD AND REMOVE TRUSTED HOSTS TO SERVERS #####################
+####                                                                        ####
+#### Module to add/remove a TrustedHost to TrustedHosts for remote          ####
+#### management   														    ####
+####																		####
+#### Use: 																	####
+####   Place PSM1 in folder : 												####
+####		C:\Program Files\WindowsPowerShell\Modules\<YOURFOLDERNAME>		####
+####																		####
+####   Import-Module <YOURFOLDERNAME>										####
+####																		####
+####   New-TrustedHost <FQDN to be trusted>           						####
+####																		####
+####   Remove-TrustedHost <FQDN to be untrusted>				    	    ####
+####																	    ####
+################################################################################
+
+
+function New-TrustedHost 
+{
+	Param
+	(
+		[parameter(Mandatory=$true)]
+		[String]$RemoteDomain
+	)
+$TrustedHostsGW = $(Get-Item WSMAN:\Localhost\Client\TrustedHosts).value
+$NewTrustedHost = set-item wsman:\localhost\Client\TrustedHosts -value "$TrustedHostsGW, $RemoteDomain" -Force
+}
+
+function Remove-TrustedHost
+{
+	Param
+	(
+		[parameter(Mandatory=$true)]
+		[String]$RemoteDomain
+	)
+
+
+$TrustedHosts = ((Get-Item WSMan:\localhost\Client\TrustedHosts).Value).Replace("$RemoteDomain,","")
+Set-Item WSMan:\localhost\Client\TrustedHosts $TrustedHosts -force
+}
